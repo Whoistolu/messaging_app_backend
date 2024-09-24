08b248f06778ee12ea6885a375cf32d129febe2b
Whoistolu <digittolu25@gmail.com>
2024-09-24 23:26:26 +0100

    Fix chat room association

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,4 +1,5 @@
 class ChatRoom < ApplicationRecord
-    belongs_to :user
-    has_many :messages
+    has_many :messages, dependent: :destroy
+    has_many :chat_room_memberships, dependent: :destroy
+    has_many :users, through: :chat_room_memberships
 end
