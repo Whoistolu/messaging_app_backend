75a3834ca280971ee8d22758d0b8f7b3224e32a7
Whoistolu <digittolu25@gmail.com>
2024-09-25 23:52:28 +0100

    Add the set_chat_room as a private method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,6 +16,10 @@ class MessagesController < ApplicationController
 
   private
 
+  def set_chat_room
+    @chat_room = ChatRoom.find(params[:chat_room_id])
+  end
+
   def message_params
     params.require(:message).permit(:content)
   end
