dc5d5fe1a8f8c76859f6e5bdf625d011872982cb
Whoistolu <digittolu25@gmail.com>
2024-09-25 23:50:33 +0100

    Add a create action to the Messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,15 @@ class MessagesController < ApplicationController
       @messages = @chat_room.messages
       render json: @messages
   end
+
+  def create
+    @message = @chat_room.messages.new(message_params)
+    @message.user = current_user
+    if @message.save
+      render json: @message, status: :created
+    else
+      render json: @message.errors, status: :unprocessable_entity
+    end
+  end
+  
 end
