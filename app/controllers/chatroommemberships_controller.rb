7ada45959df94f2dfc5e966b33bb3812fedf524c
Whoistolu <digittolu25@gmail.com>
2024-09-26 22:52:54 +0100

    Add Create action to this controller and placed the params under a private method

--- app/controllers/chatroommemberships_controller.rb
+++ app/controllers/chatroommemberships_controller.rb
@@ -1,4 +1,16 @@
 class ChatRoomMembershipsController < ApplicationController
+
+  def create
+    @chat_room_membership = ChatRoomMembership.new(membership_params)
+    if @chat_room_membership.save
+      render json: @chat_room_membership, status: :created
+    else
+      render json: @chat_room_membership.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
   def membership_params
     params.require(:chat_room_membership).permit(:user_id, :chat_room_id)
   end
