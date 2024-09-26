844fd08636d5893656d4858eefcf6fff7fdb87a1
Whoistolu <digittolu25@gmail.com>
2024-09-26 22:53:32 +0100

    Add destroy action to this controller

--- app/controllers/chatroommemberships_controller.rb
+++ app/controllers/chatroommemberships_controller.rb
@@ -9,6 +9,16 @@ class ChatRoomMembershipsController < ApplicationController
     end
   end
 
+  def destroy
+    @chat_room_membership = ChatRoomMembership.find_by(user_id: params[:user_id], chat_room_id: params[:chat_room_id])
+    if @chat_room_membership
+      @chat_room_membership.destroy
+      head :no_content
+    else
+      render json: { error: 'Membership not found' }, status: :not_found
+    end
+  end
+
   private
 
   def membership_params
