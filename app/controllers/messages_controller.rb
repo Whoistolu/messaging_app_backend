12a83c9684e9e3c06ea0203ea59a745ee333b015
Whoistolu <digittolu25@gmail.com>
2024-09-27 23:38:40 +0100

    Remove index and create action from the Messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,5 @@
 class MessagesController < ApplicationController
-  def index
-      @messages = @chat_room.messages
-      render json: @messages
-  end
-
-  def create
-    @message = @chat_room.messages.new(message_params)
-    @message.user = current_user
-    if @message.save
-      render json: @message, status: :created
-    else
-      render json: @message.errors, status: :unprocessable_entity
-    end
-  end
-
+  
   private
 
   def set_chat_room
