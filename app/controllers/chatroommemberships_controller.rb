332f0dd2c3fe1670a19829f3eb936c5c707b2255
Whoistolu <digittolu25@gmail.com>
2024-09-27 23:31:11 +0100

    Remove the create action from the same controller

--- app/controllers/chatroommemberships_controller.rb
+++ app/controllers/chatroommemberships_controller.rb
@@ -1,14 +1,4 @@
 class ChatRoomMembershipsController < ApplicationController
-
-  def create
-    @chat_room_membership = ChatRoomMembership.new(membership_params)
-    if @chat_room_membership.save
-      render json: @chat_room_membership, status: :created
-    else
-      render json: @chat_room_membership.errors, status: :unprocessable_entity
-    end
-  end
-
   private
 
   def membership_params
