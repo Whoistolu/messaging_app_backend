1ef69d8e234858d9f6b28d3a82610e3e0ad95efb
Whoistolu <digittolu25@gmail.com>
2024-09-27 23:33:42 +0100

    Remove create action from the Chat Rooms controller

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -4,15 +4,6 @@ class ChatRoomsController < ApplicationController
    def index
       @chat_rooms = ChatRoom.all
       render json: @chat_rooms
-      end
-
-   def create
-      @chat_room = ChatRoom.new(chat_room_params)
-      if @chat_room.save
-         render json: @chat_room, status: :created
-      else
-         render json: @chat_room.errors, status: :unprocessable_entity
-      end
    end
 
    def show
