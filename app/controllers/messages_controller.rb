4de506b330e189ce3810d63b84bb756b940389f5
Whoistolu <digittolu25@gmail.com>
2024-09-27 23:40:38 +0100

    Remove the param actions from the private method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,2 @@
 class MessagesController < ApplicationController
-  
-  private
-
-  def set_chat_room
-    @chat_room = ChatRoom.find(params[:chat_room_id])
-  end
-
-  def message_params
-    params.require(:message).permit(:content)
-  end
-
 end
