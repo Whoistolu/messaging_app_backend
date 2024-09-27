6280171881502bd3da73dff2d2b2ff837910b10a
Whoistolu <digittolu25@gmail.com>
2024-09-27 23:37:15 +0100

    Remove the index action and the params in the private method

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,18 +1,2 @@
 class ChatRoomsController < ApplicationController
-   before_action :set_chat_room
-
-   def index
-      @chat_rooms = ChatRoom.all
-      render json: @chat_rooms
-   end
-
-   private
-
-   def set_chat_room
-     @chat_room = ChatRoom.find(params[:id])
-   end
- 
-   def chat_room_params
-     params.require(:chat_room).permit(:name, :is_private)
-   end
 end
