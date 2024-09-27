d667ff8d26f510ffb6623224da6498d0db7b03c2
Whoistolu <digittolu25@gmail.com>
2024-09-27 23:32:01 +0100

    Remove the membership params from the private method of the controller

--- app/controllers/chatroommemberships_controller.rb
+++ app/controllers/chatroommemberships_controller.rb
@@ -1,7 +1,2 @@
 class ChatRoomMembershipsController < ApplicationController
-  private
-
-  def membership_params
-    params.require(:chat_room_membership).permit(:user_id, :chat_room_id)
-  end
 end
