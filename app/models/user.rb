23c636c2bfb0d3ad570808a6e1962f4bbbd662b9
Whoistolu <digittolu25@gmail.com>
2024-09-28 12:26:40 +0100

    Remove Devise Code from Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,2 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  has_many :chat_room_memberships, dependent: :destroy
-  has_many :chat_rooms, through: :chat_room_memberships
-  has_many :messages, dependent: :destroy
 end
