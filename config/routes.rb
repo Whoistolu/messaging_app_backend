fc0b9f4bcadffb4a508124ce7eca6e18c836549a
Whoistolu <digittolu25@gmail.com>
2024-09-28 12:15:19 +0100

    Remove message and the chat room membership routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :chat_rooms, only: [:index, :show, :create, :destroy] do
-    resources :messages, only: [:index, :create]
-    resources :chat_room_memberships, only: [:create, :destroy]
+    
   end
 end
