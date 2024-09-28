937e8827ca467be2d6d9bc5592b8a04a47a10da7
Whoistolu <digittolu25@gmail.com>
2024-09-28 12:30:29 +0100

    Remove chat room association

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,5 +1,2 @@
 class ChatRoom < ApplicationRecord
-    has_many :messages, dependent: :destroy
-    has_many :chat_room_memberships, dependent: :destroy
-    has_many :users, through: :chat_room_memberships
 end
